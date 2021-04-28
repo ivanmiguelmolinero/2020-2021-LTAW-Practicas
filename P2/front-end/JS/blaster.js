91f0b4832b06884570e29a03b1bd6c912ab1058a
ivanmiguelmolinero <i.miguel.molinero@gmail.com>
2021-04-28 18:28:42 +0200

    Barra de búsqueda funcional en todas las páginas

--- P2/front-end/JS/blaster.js
+++ P2/front-end/JS/blaster.js
@@ -3,12 +3,14 @@ function main() {
     // Obtenemos los audios
     var audioxwing = document.getElementById("audioxwing");
     var audiosable = document.getElementById("audiosable");
-    var audiocarrito = document.getElementById("audiocarrito");
-    var audiocompra =document.getElementById("audiocompra");
 
     // Obtenemos los productos
     var xwing = document.getElementById("X-Wing");
     var sable = document.getElementById("sable");
+
+    //Obtengo la caja de búsqueda y el display
+    var caja = document.getElementById("caja_busqueda");
+    var display = document.getElementById("display");
 
     xwing.onmouseover = () => {
         audioxwing.play();
@@ -16,5 +18,65 @@ function main() {
 
     sable.onmouseover = () => {
         audiosable.play();
+    }
+
+    //-- Retrollamda del boton de Ver productos
+    caja.oninput = () => {
+
+        //-- Crear objeto para hacer peticiones AJAX
+        const m = new XMLHttpRequest();
+
+        //-- Función de callback que se invoca cuando
+        //-- hay cambios de estado en la petición
+        m.onreadystatechange = () => {
+
+            //-- Petición enviada y recibida. Todo OK!
+            if (m.readyState==4) {
+
+                //-- Solo la procesamos si la respuesta es correcta
+                if (m.status==200) {
+
+                    //-- La respuesta es un objeto JSON
+                    let productos = JSON.parse(m.responseText)
+
+                    console.log(productos);
+
+                    //-- Borrar el resultado anterior
+                    display.innerHTML = "";
+
+                    //--Recorrer los productos del objeto JSON
+                    for (let i=0; i < productos.length; i++) {
+
+                        //-- Añadir cada producto al párrafo de visualización
+                        display.innerHTML += productos[i];
+
+                        //-- Separamos los productos por ',''
+                        if (i < productos.length-1) {
+                        display.innerHTML += '<br>';
+                        }
+                    }
+
+                } else {
+                    //-- Hay un error en la petición
+                    //-- Lo notificamos en la consola y en la propia web
+                    console.log("Error en la petición: " + m.status + " " + m.statusText);
+                }
+            }
+        }
+
+        console.log(caja.value.length);
+
+        //-- La peticion se realia solo si hay al menos 1 carácter
+        if (caja.value.length >= 1) {
+
+        //-- Configurar la petición
+        m.open("GET","/productos?param1=" + caja.value, true);
+
+        //-- Enviar la petición!
+        m.send();
+        
+        } else {
+            display.innerHTML="";
+        }
     }
 }
--- P2/front-end/JS/sable.js
+++ P2/front-end/JS/sable.js
@@ -10,6 +10,9 @@ function main() {
     var blaster = document.getElementById("blaster");
     var xwing = document.getElementById("XWing");
 
+    //Obtengo la caja de búsqueda y el display
+    var caja = document.getElementById("caja_busqueda");
+    var display = document.getElementById("display");
 
     blaster.onmouseover = () => {
         audioblaster.play();
@@ -18,4 +21,65 @@ function main() {
     xwing.onmouseover = () => {
         audioxiwing.play();
     }
+
+    //-- Retrollamda del boton de Ver productos
+    caja.oninput = () => {
+
+        //-- Crear objeto para hacer peticiones AJAX
+        const m = new XMLHttpRequest();
+
+        //-- Función de callback que se invoca cuando
+        //-- hay cambios de estado en la petición
+        m.onreadystatechange = () => {
+
+            //-- Petición enviada y recibida. Todo OK!
+            if (m.readyState==4) {
+
+                //-- Solo la procesamos si la respuesta es correcta
+                if (m.status==200) {
+
+                    //-- La respuesta es un objeto JSON
+                    let productos = JSON.parse(m.responseText)
+
+                    console.log(productos);
+
+                    //-- Borrar el resultado anterior
+                    display.innerHTML = "";
+
+                    //--Recorrer los productos del objeto JSON
+                    for (let i=0; i < productos.length; i++) {
+
+                        //-- Añadir cada producto al párrafo de visualización
+                        display.innerHTML += productos[i];
+
+                        //-- Separamos los productos por ',''
+                        if (i < productos.length-1) {
+                        display.innerHTML += '<br>';
+                        }
+                    }
+
+                } else {
+                    //-- Hay un error en la petición
+                    //-- Lo notificamos en la consola y en la propia web
+                    console.log("Error en la petición: " + m.status + " " + m.statusText);
+                }
+            }
+        }
+
+        console.log(caja.value.length);
+
+        //-- La peticion se realia solo si hay al menos 1 carácter
+        if (caja.value.length >= 1) {
+
+        //-- Configurar la petición
+        m.open("GET","/productos?param1=" + caja.value, true);
+
+        //-- Enviar la petición!
+        m.send();
+        
+        } else {
+            display.innerHTML="";
+        }
+    }
+
 }
